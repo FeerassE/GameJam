5ff9ba9c70c5a23dc2c3aa598e0ed63156bfe319
Dillon <dillon@nixx.co>
2020-04-18 14:31:50 -0400

    Recipes

---
+++ Assets/Scripts/Memory.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Memory : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed = 0.05f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.right * speed * Time.deltaTime;
+        if (this.transform.position.x > 50)
+            Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/MemorySpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemorySpawner : MonoBehaviour
+{
+    public GameObject happy;
+    public GameObject sad;
+    public GameObject angry;
+    public GameObject depression;
+    public GameObject anxiety;
+    public GameObject empathy;
+    public float respawnTime = 5.0f;
+    public Stack<GameObject> numbers = new Stack<GameObject>();
+
+    void Start()
+    {
+        
+        numbers.Push(happy);
+        numbers.Push(sad);
+        numbers.Push(angry);
+        numbers.Push(depression);
+        numbers.Push(anxiety);
+        numbers.Push(empathy);
+        spawnMemory();
+        StartCoroutine(memories());
+    }
+
+    public void spawnMemory()
+    {
+        GameObject a = (GameObject)Instantiate(numbers.Pop(), new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+    }
+
+    IEnumerator memories()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(respawnTime);
+            spawnMemory();
+        }
+    }
+}
