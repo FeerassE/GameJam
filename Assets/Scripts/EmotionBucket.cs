892e585d7ddd932e6e005d24a6a4e1c2728de06f
Dillon <dillon@nixx.co>
2020-04-18 19:53:24 -0400

    Create all emotion barrels and add to the scene

---
+++ Assets/Scripts/EmotionBucket.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmotionBucket : MonoBehaviour
+{
+    public GameObject bucket;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject a = (GameObject)Instantiate(bucket, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "emotion") {
+            StartCoroutine(memories());
+        }
+    }
+
+    IEnumerator memories()
+    {
+        yield return new WaitForSeconds(2.0f);
+        GameObject a = (GameObject)Instantiate(bucket, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+
+    }
+}
