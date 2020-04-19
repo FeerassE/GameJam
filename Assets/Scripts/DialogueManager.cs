a522088e83091b25dc4e4d4d6c6f393cc0d97329
Dillon <dillon@nixx.co>
2020-04-19 14:49:46 -0400

    Add dialogue

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    private Queue<string> sentences;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        sentences.Clear();
+
+        foreach(string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+    }
+
+    void EndDialogue()
+    {
+
+    }
+
+}
