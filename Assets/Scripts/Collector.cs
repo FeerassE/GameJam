d969be6484202c60be3d5dcb0123d5b9251475b1
Feerass Ellid <FeerassE@gmail.com>
2020-04-19 14:36:52 -0400

    randomizing recipes, only certain number at a time, shrinking collector,

--- Assets/Scripts/Collector.cs
+++ Assets/Scripts/Collector.cs
@@ -12,29 +12,46 @@ public class Collector : MonoBehaviour
     public Vector2 size;
     public float width;
     public float height;
+
+    public float shrinkSpeed = 2.0f;
+    public float shrinkAmount = 0.4f;
+    public int maxShrinking = 50;
+
+    public int timesShrunk = 0;
     void Start() {
         memorySpawner = GameObject.Find("RecipeSpawner");
         size = gameObject.GetComponent<SpriteRenderer>().size; 
         width = size.x;
         height = size.y;   
+
+        /* shrinks at shrinkspeed starting in 5 seconds*/
+        InvokeRepeating("ShrinkCollector", 5.0f,  shrinkSpeed);
     } 
     void OnTriggerEnter2D(Collider2D coll)
     {   
         if(coll.gameObject.tag == "emotion") {
-            if(coll.gameObject.GetComponent<Memory>().type == memorySpawner.GetComponent<MemorySpawner>().activeMemories.Peek().GetComponent<Memory>().type) {
-                Destroy(memorySpawner.GetComponent<MemorySpawner>().activeMemories.Dequeue());
-                points++;
-                gameObject.transform.localScale += new Vector3(width + 1, height + 1, 0);
-                Destroy(coll.gameObject);
+            if(memorySpawner.GetComponent<MemorySpawner>().activeMemories.Count > 0) {
+                if(coll.gameObject.GetComponent<Memory>().type == memorySpawner.GetComponent<MemorySpawner>().activeMemories.Peek().GetComponent<Memory>().type) {
+                    Debug.Log("Deleting");
+                    Destroy(memorySpawner.GetComponent<MemorySpawner>().activeMemories.Dequeue());
+                    points++;
+                    gameObject.transform.localScale += new Vector3(width + 1, height + 1, 0);
+                    Destroy(coll.gameObject);
+                }
             }
             if(coll.gameObject) {
                 Destroy(coll.gameObject);
             }
         }
     }
-    // Start is called before the first frame update
 
-    // Update is called once per frame
+    /* shrinks the collector */
+    void ShrinkCollector() {
+        if(timesShrunk != maxShrinking) {
+            gameObject.transform.localScale += new Vector3(width - shrinkAmount, height - shrinkAmount, 0);
+            timesShrunk++;
+        }
+    }
     void Update()
     {
         
